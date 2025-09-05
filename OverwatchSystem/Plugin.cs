4e548f839358d58746cec318a0f1b306747cd5a7
DiabeloDev <diabelodev@gmail.com>
2025-09-06 01:43:21 +0200

    Bump Exiled 9.8.1

--- OverwatchSystem/Plugin.cs
+++ OverwatchSystem/Plugin.cs
@@ -8,8 +8,8 @@ namespace OverwatchSystem
     {
         public override string Author { get; } = ".Diabelo";
         public override string Name { get; } = "OverwatchSystem";
-        public override Version Version => new Version(1, 0, 4);
-        public override Version RequiredExiledVersion { get; } = new Version(9, 6, 1);
+        public override Version Version => new Version(1, 0, 5);
+        public override Version RequiredExiledVersion { get; } = new Version(9, 8, 1);
         public override PluginPriority Priority { get; } = PluginPriority.Higher;
         public static Plugin Instance { get; private set; }
         public override async void OnEnabled()
